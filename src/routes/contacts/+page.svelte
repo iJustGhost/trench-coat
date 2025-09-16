edc5f07470a4a1ba0e33bfb1386d2afc4ef1ac11
iJustGhost <fryingchicken81@gmail.com>
2025-09-17 00:12:37 +0800

    implemented current identity info

--- src/routes/contacts/+page.svelte
+++ src/routes/contacts/+page.svelte
@@ -1,51 +1,60 @@
 <script>
 	import { goto } from '$app/navigation';
-    import { getUsername } from '$lib/openpgp';
+	import { armoredPublicKey, comment, email, username } from '$lib/openpgp';
 
-    /** @type {import('./$types').PageProps} */
-    let { data } = $props();
+	/** @type {import('./$types').PageProps} */
+	let { data } = $props();
 
+	function copyPublicKey() {
+		navigator.clipboard.writeText(armoredPublicKey);
+	}
 </script>
 
-<p class="text-6xl text-center">{getUsername()}</p>
-
-<div class="flex items-center m-2">
-    <p>Public Key: cf44f87d-4967-448b-93d6-17385f5714c5</p>
-    <button class="bg-green-700 cursor-pointer p-1 ml-2 text-sm">Copy</button>
+<div class="mx-4 my-4">
+	<p class="mb-2 flex text-5xl">{username}</p>
+	<div class="flex items-center justify-between">
+		<div class="flex">
+			<p>{email}</p>
+			<p class="ml-2">({comment})</p>
+		</div>
+		<button class="ml-2 cursor-pointer bg-green-700 p-1 text-sm" onclick={copyPublicKey}
+			>Copy Public Key</button
+		>
+	</div>
 </div>
 
-<p class="text-2xl">Contacts</p>
-<div class="mx-8 my-2 overflow-auto h-72">
-    <div class="contact">
-        <p class="username">Lorem ipsum</p>
-        <p class="email">Lorem ipsum@mail.com</p>
-    </div>
-    <div class="contact">
-        <p class="username">Lorem ipsum</p>
-        <p class="email">Lorem ipsum@mail.com</p>
-    </div>
-    <div class="contact">
-        <p class="username">Lorem ipsum</p>
-        <p class="email">Lorem ipsum@mail.com</p>
-    </div>
-    <div class="contact">
-        <p class="username">Lorem ipsum</p>
-        <p class="email">Lorem ipsum@mail.com</p>
-    </div>
-    <div class="contact">
-        <p class="username">Lorem ipsum</p>
-        <p class="email">Lorem ipsum@mail.com</p>
-    </div>
-    <div class="contact">
-        <p class="username">Lorem ipsum</p>
-        <p class="email">Lorem ipsum@mail.com</p>
-    </div>
-    <div class="contact">
-        <p class="username">Lorem ipsum</p>
-        <p class="email">Lorem ipsum@mail.com</p>
-    </div>
-    <div class="contact">
-        <p class="username">Lorem ipsum</p>
-        <p class="email">Lorem ipsum@mail.com</p>
-    </div>
+<p class="text-2xl ml-4">Contacts</p>
+<div class="mx-8 my-2 h-72 overflow-auto">
+	<div class="contact">
+		<p class="username">Lorem ipsum</p>
+		<p class="email">Lorem ipsum@mail.com</p>
+	</div>
+	<div class="contact">
+		<p class="username">Lorem ipsum</p>
+		<p class="email">Lorem ipsum@mail.com</p>
+	</div>
+	<div class="contact">
+		<p class="username">Lorem ipsum</p>
+		<p class="email">Lorem ipsum@mail.com</p>
+	</div>
+	<div class="contact">
+		<p class="username">Lorem ipsum</p>
+		<p class="email">Lorem ipsum@mail.com</p>
+	</div>
+	<div class="contact">
+		<p class="username">Lorem ipsum</p>
+		<p class="email">Lorem ipsum@mail.com</p>
+	</div>
+	<div class="contact">
+		<p class="username">Lorem ipsum</p>
+		<p class="email">Lorem ipsum@mail.com</p>
+	</div>
+	<div class="contact">
+		<p class="username">Lorem ipsum</p>
+		<p class="email">Lorem ipsum@mail.com</p>
+	</div>
+	<div class="contact">
+		<p class="username">Lorem ipsum</p>
+		<p class="email">Lorem ipsum@mail.com</p>
+	</div>
 </div>
