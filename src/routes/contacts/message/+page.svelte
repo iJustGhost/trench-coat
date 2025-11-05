c3505d5475bcd7b94abea84ece349476c6ee0e86
iJustGhost <fryingchicken81@gmail.com>
2025-11-06 00:09:37 +0800

    placeholder for message encryption ui

---
+++ src/routes/contacts/message/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { goto } from '$app/navigation';
+    const params = new URLSearchParams(window.location.search);
+    
+</script>
+
+<p>{params.get('name')}</p>
+<p>{params.get('email')}</p>
+<p>{params.get('filename')}</p>
+
+<button onclick={() => {
+    goto('/contacts')
+}}>Return</button>
